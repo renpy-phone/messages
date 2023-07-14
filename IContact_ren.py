533ebc603c188c1898a5789316f68c1a05ab9e7b
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2023-07-15 00:57:04 +0100

    add: Added profile picture to IContact

--- IContact_ren.py
+++ IContact_ren.py
@@ -1,13 +1,15 @@
 from Message_ren import Message
 
 """renpy
-init python:
+init -100 python:
 """
 
 from abc import ABC
 
 
 class IContact(ABC):
-    def __init__(self) -> None:
+    def __init__(self, profile_picture: str) -> None:
+        self.profile_picture: str = profile_picture
+
         self.text_messages: list[Message] = []
         self.pending_text_messages: list[Message] = []
